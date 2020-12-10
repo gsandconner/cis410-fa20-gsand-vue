dc4d3958576f780ada557e6ae9862c30ad055fcd
cgsand <gsandconner@gmail.com>
2020-12-10 12:35:18 -0700

    unfinished store

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
-
+import store from './store.js'
 import router from './routes.js'
 
 axios.defaults.baseURL = 'https://cis410-fa20-gsand2-api.azurewebsites.net'
@@ -10,5 +10,6 @@ axios.defaults.baseURL = 'https://cis410-fa20-gsand2-api.azurewebsites.net'
 new Vue({
   el: '#app',
   router,
+  store:store,
   render: h => h(App)
 })
---
+++ src/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state:{
+token:null,
+user:null,
+Stickers:[]
+    },
+    mutations:{
+storeTokenInApp(state, myToken){
+    state.token=myToken;
+},
+storeTokenInApp(state,myUser){
+    state.user=myUser 
+}
+    },
+    actions:{
+
+    }
+})
